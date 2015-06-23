a73e77a9726c42df7b4e881739ebea4720b2f763
Jorge Meireles <jmeireles@rocket-internet.pt>
2015-06-23 09:46:04 +0100

    first design patterns

---
+++ Factory.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package    designpatterns
+ * @category   designpatterns
+ * @author     Jorge Meireles
+ * @copyright  (c) 2015 Jovago
+ */
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+/**
+ * Class AutomobileFactory
+ */
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
